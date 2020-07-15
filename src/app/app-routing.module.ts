0804bab33baf5fdd0d6e52e487f9c1cfeca5bc0c
Juveria Manzar <juveriamanzar29@gmail.com>
2020-07-16 04:20:11 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+    {path: 'details/:city', component: DetailsComponent},
+    // {path: 'add', component: AddComponent},
+    // {path: 'login', component: LoginComponent},
+    // {path: 'signup', component: SignupComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
