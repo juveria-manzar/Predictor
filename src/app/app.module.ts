6602996a49e0bcafeb23452d28ff121df64c8c0c
Juveria Manzar <juveriamanzar29@gmail.com>
2020-07-22 00:41:59 +0530

    create guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { AddCardComponent } from './add-card/add-card.component';
 import { DetailsComponent } from './details/details.component';
+import {AngularFireLite} from 'angularfire-lite';
+import {environment} from '../environments/environment';
 import {NguiAutoCompleteModule} from '@ngui/auto-complete';
 import { AddComponent } from './add/add.component';
+import {FormsModule} from '@angular/forms';
+// import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -20,9 +25,13 @@ import { AddComponent } from './add/add.component';
     AddComponent
   ],
   imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    HttpClientModule,
     NguiAutoCompleteModule,
-    BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AngularFireLite.forRoot(environment.config),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
