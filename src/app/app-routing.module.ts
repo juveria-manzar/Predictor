757f1c8794f963f2353bd04b20f09f3197427fcb
Juveria Manzar <juveriamanzar29@gmail.com>
2020-07-24 02:56:11 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import {AddComponent} from './add/add.component'
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
     {path: 'details/:city', component: DetailsComponent},
-    // {path: 'add', component: AddComponent},
+    {path: 'add', component: AddComponent},
     // {path: 'login', component: LoginComponent},
     // {path: 'signup', component: SignupComponent},
+    {path: '**', redirectTo: ''}
 
 ];
 
